7a5a0afc29e4e2864fd7aa50878f0a2ce18e1ad9
Bryant Biggs <bryantbiggs@gmail.com>
2020-09-23 21:03:02 -0400

    feat: initial action to evaluate pull request titles against patterns

---
+++ pr-title/README.md
@@ -0,0 +1,49 @@
+<h1 style="font-size: 56px; margin: 0; padding: 0;" align="center">
+  pr-title
+</h1>
+<p align="center">
+  <img src="https://github.com/clowdhaus/actions/workflows/pr-title/badge.svg" alt="pr-title">
+</p>
+
+GitHub action for checking pull request titles against a provided pattern - commonly used to remind users to add Jira ticket IDs to their PR title and/or enforcing semantic titles that follow [conventional-commits](https://www.conventionalcommits.org)
+
+## Usage
+
+```yml
+- uses: clowdhaus/actions/pr-title@master
+  with:
+    # GitHub token which contains access to the repository
+    github-token: ${{ secrets.GITHUB_TOKEN }}
+    # Regex pattern that PR title is evaluated against
+    title-regex: ''
+    # Determines whether changes are required when the title does not match the pattern
+    on-fail-request-changes: true|false
+    # Determines whether a comment should be left when the title does not match the pattern
+    on-fail-add-comment: true|false
+    # Message posted to user via requested changes or through a comment
+    on-fail-message: ''
+    # Determines whether the action should be marked as failed when title does not match pattern
+    on-fail-fail-action: true|false
+```
+
+## Scenarios
+
+### Comment on pull request when the title does not match the default pattern provided
+
+```yml
+- uses: clowdhaus/actions/pr-title@master
+  with:
+    github-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+### Request changes on pull request when the title does not match the pattern provided
+
+```yml
+- uses: clowdhaus/actions/pr-title@master
+  with:
+    title-regex: '^\[JIRA-[0-9]{1,4}]'
+    on-fail-request-changes: true
+    on-fail-add-comment: false
+    on-fail-message: 'Your pull request title does not match the provided pattern: `%regex%`'
+    github-token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ pr-title/index.ts
@@ -0,0 +1,71 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const githubToken = core.getInput('github-token', { required: true });
+const titleRegexInput = core.getInput('title-regex');
+const onFailrequestChanges = core.getInput('on-fail-request-changes') == 'true';
+const onFailAddComment = core.getInput('on-fail-add-comment') == 'true';
+const onFailMessage = core.getInput('on-fail-message');
+const onFailFailAction = core.getInput('on-fail-fail-action') == 'true';
+
+const octokit = github.getOctokit(githubToken);
+
+async function run(): Promise<void> {
+  const githubContext = github.context;
+  const pullRequest = githubContext.issue;
+
+  const titleRegex = new RegExp(titleRegexInput);
+  const title: string = (githubContext.payload.pull_request?.title as string) ?? '';
+  const comment = onFailMessage.replace('%regex%', titleRegex.source);
+
+  core.debug(`Title Regex: ${titleRegex.source}`);
+  core.debug(`Title: ${title}`);
+
+  const titleMatchesRegex: boolean = titleRegex.test(title);
+  if (!titleMatchesRegex) {
+    if (onFailrequestChanges || onFailAddComment) {
+      createReview(comment, pullRequest);
+    }
+    if (onFailFailAction) {
+      core.setFailed(comment);
+    }
+  } else {
+    if (onFailrequestChanges) {
+      await dismissReview(pullRequest);
+    }
+  }
+}
+
+function createReview(comment: string, pullRequest: { owner: string; repo: string; number: number }) {
+  void octokit.pulls.createReview({
+    owner: pullRequest.owner,
+    repo: pullRequest.repo,
+    pull_number: pullRequest.number,
+    body: comment,
+    event: onFailAddComment ? 'COMMENT' : 'REQUEST_CHANGES',
+  });
+}
+
+async function dismissReview(pullRequest: { owner: string; repo: string; number: number }) {
+  const reviews = await octokit.pulls.listReviews({
+    owner: pullRequest.owner,
+    repo: pullRequest.repo,
+    pull_number: pullRequest.number,
+  });
+
+  reviews.data.forEach((review) => {
+    if (review.user.login == 'github-actions[bot]') {
+      void octokit.pulls.dismissReview({
+        owner: pullRequest.owner,
+        repo: pullRequest.repo,
+        pull_number: pullRequest.number,
+        review_id: review.id,
+        message: 'All good!',
+      });
+    }
+  });
+}
+
+run().catch((error) => {
+  core.setFailed(error);
+});
